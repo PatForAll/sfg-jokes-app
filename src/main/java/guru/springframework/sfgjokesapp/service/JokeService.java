d363430bc0e14025e45903d263245c34f7ab7bc3
PatForAll <siekmeier.p@gmail.com>
2021-07-10 18:12:44 +0200

    implement joke service, controller & template

---
+++ src/main/java/guru/springframework/sfgjokesapp/service/JokeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgjokesapp.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+public interface JokeService {
+
+    String tellJoke();
+}
